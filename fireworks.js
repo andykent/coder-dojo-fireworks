1715da42a5c84cbf9172bdf05c1da480feceee2f
Andy Kent <andy.kent@me.com>
2012-10-30 21:27:57 +0000

    added a setup method fireworks.display(fn)

--- fireworks.js
+++ fireworks.js
@@ -3,8 +3,15 @@ window.fireworks = f = {
 
   // DSL
 
-  setup: function() {
-    Fireworks.initialize();
+  display: function(fn) {
+    window.onload = function() {
+      f.loadCSS('styles.css');
+      f.loadImages();
+      f.loadJS('creativeFireworks.js', function() {
+        Fireworks.initialize();
+        fn.call(this, f);
+      });
+    }
   },
 
   launch: function(opts) {
@@ -44,4 +51,26 @@ window.fireworks = f = {
     return function() { f.launch(opts) };
   },
 
+  loadCSS: function(filename) {
+    var css = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("href", filename);
+    document.getElementsByTagName("head")[0].appendChild(css);
+  },
+
+  loadJS: function(filename, fn) {
+    var js = document.createElement('script')
+    js.setAttribute("type","text/javascript")
+    js.setAttribute("src", filename)
+    js.onload = fn;
+    document.getElementsByTagName("head")[0].appendChild(js);
+  },
+
+  loadImages: function() {
+    var aside = document.createElement('aside');
+    aside.setAttribute('id', 'library');
+    aside.innerHTML = '<img src="images/nightsky.png" id="nightsky" /><img src="images/big-glow.png" id="big-glow" /><img src="images/small-glow.png" id="small-glow" />';
+    document.getElementsByTagName("body")[0].appendChild(aside);
+  },
 };
